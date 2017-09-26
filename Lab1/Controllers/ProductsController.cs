2f122dbd1bcea5574ebd51e806ff7df1a0079853
ya-ya-it <ya-ya-it@yandex.com>
2017-09-25 20:13:32 -0400

    initial commit

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Details(string details)
+        {
+            ViewBag.Details = "You selected product: " + details ;
+            return View();
+        }
+
+    // GET: Products
+    public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
